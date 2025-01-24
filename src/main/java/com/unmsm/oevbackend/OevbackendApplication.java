15a274716575908e9dad8026ff9a8e1ab7bdfb8a
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-01-24 16:16:51 -0500

    first commit

---
+++ src/main/java/com/unmsm/oevbackend/OevbackendApplication.java
@@ -0,0 +1,13 @@
+package com.unmsm.oevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OevbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OevbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unmsm/oevbackend/OevbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unmsm.oevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OevbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
